96fa2793047642ebb668b0423419868dc7ba29ab
malufrt <31512601+malufrt@users.noreply.github.com>
2017-11-06 22:11:55 -0200

    Update Modelo Físico.sql

--- Modelo Físico.sql
+++ Modelo Físico.sql
@@ -59,8 +59,8 @@ CREATE TABLE Passagem
     ID_passagem INTEGER PRIMARY KEY,
     ID_rota INTEGER,
     ID_companhia INTEGER,
-    Data_compra VARCHAR[30],
-    Data_viagem VARCHAR[30],
+    Data_compra DATE,
+    Data_viagem DATE,
     preco FLOAT
 );
 
@@ -79,7 +79,7 @@ CREATE TABLE Rodoviaria
 (
     ID_rodoviaria INTEGER PRIMARY KEY,
     ID_empresa INTEGER,
-    Horario_de_funcionamento VARCHAR[30]
+    Horario_de_funcionamento TIME
 );
  
  
