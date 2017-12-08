0450c4aa1c2cd56e1a608e6c0139c3609f9a8604
malufrt <31512601+malufrt@users.noreply.github.com>
2017-12-08 19:06:09 -0200

    Update Modelo Físico.sql

--- Arquivos SQL/Modelo Físico.sql
+++ Arquivos SQL/Modelo Físico.sql
@@ -2,115 +2,126 @@
 
 
 /* Cria tabela PESSOA */
-CREATE TABLE PESSOA 
+CREATE TABLE PESSOA
 (
-    ID_pessoa INTEGER PRIMARY KEY,
-    Nome VARCHAR[30],
-    Identidade VARCHAR[30],
-    CPF VARCHAR[30],
-    Nacionalidade VARCHAR[30],
-    Telefone VARCHAR[30],
-    Email VARCHAR[30],
-    Data_de_nascimento DATE
+	ID_pessoa INTEGER PRIMARY KEY,
+	Nome VARCHAR[30],
+	Identidade VARCHAR[30],
+	CPF VARCHAR[30],
+	Data_de_nascimento DATE,
+	Nacionalidade VARCHAR[30]
 );
+
+
+
+/* Cria tabela PESSOA_FUNCIONARIO */
+CREATE TABLE PESSOA_FUNCIONARIO
+(
+	ID_pessoa INTEGER,
+	ID_empresa INTEGER
+
+	FOREIGN KEY (ID_pessoa)
+	REFERENCES PESSOA (ID_pessoa),
+	
+	FOREIGN KEY (ID_empresa)
+	REFERENCES EMPRESA (ID_empresa)
+);
+
 
 
 /* Cria tabela EMPRESA */
 CREATE TABLE EMPRESA 
 (
-    ID_empresa INTEGER PRIMARY KEY,
-    Nome VARCHAR[30],
-    Telefone VARCHAR[30]
+	ID_empresa INTEGER PRIMARY KEY,	
+	Nome VARCHAR[30],
+	Horario_de_funcionamento VARCHAR[30],
+	CEP INTEGER
 );
 
-
-/* Cria tabela COMPANHIA */
-CREATE TABLE COMPANHIA 
-(
-    ID_companhia INTEGER PRIMARY KEY,
-    ID_empresa INTEGER,
-    email VARCHAR[30],
-	
-	FOREIGN KEY (ID_empresa)
-    REFERENCES EMPRESA (ID_empresa)
-    
-);
-
-
-/* Cria tabela RODOVIARIA */
-CREATE TABLE RODOVIARIA 
-(
-    ID_rodoviaria INTEGER PRIMARY KEY,
-    ID_empresa INTEGER,
-    Horario_de_funcionamento TIME,
-	
-	FOREIGN KEY (ID_empresa)
-    REFERENCES EMPRESA (ID_empresa)
-    
-);
-
-
-/* Cria tabela ROTAS */
-CREATE TABLE ROTAS 
-(
-    ID_rota INTEGER PRIMARY KEY,
-    Hora_saida TIME,
-    Hora_chegada TIME,
-    ID_rodoviaria_saida INTEGER,
-    ID_rodoviaria_chegada INTEGER,
-	
-	FOREIGN KEY (ID_rodoviaria_chegada)
-    REFERENCES RODOVIARIA (ID_rodoviaria)
-	
-	FOREIGN KEY (ID_rodoviaria_saida)
-    REFERENCES RODOVIARIA (ID_rodoviaria)
-);
 
 
 /* Cria tabela PASSAGEM */
 CREATE TABLE PASSAGEM 
 (
-    ID_passagem INTEGER PRIMARY KEY,
-    ID_rota INTEGER,
-    ID_companhia INTEGER,
-    Data_compra DATE,
-    Data_viagem DATE,
-    preco FLOAT,
+	ID_passagem INTEGER PRIMARY KEY,
+	ID_rota INTEGER,
+	ID_empresa INTEGER,
+	Data_compra VARCHAR[30],
+	Data_viagem VARCHAR[30],
+	preco DOUBLE,
+	ID_pessoa INTEGER,
 	
+
 	FOREIGN KEY (ID_rota)
-    REFERENCES ROTAS (ID_rota)
+	REFERENCES ROTAS (ID_rota),
 
-    FOREIGN KEY (ID_companhia)
-    REFERENCES COMPANHIA (ID_companhia)
-    
-);
-
-
-/* Cria tabela PESSOA_PASSAGEIRO */
-CREATE TABLE PESSOA_PASSAGEIRO 
-(
-    ID_passageiro INTEGER PRIMARY KEY,
-    ID_pessoa INTEGER,
-    ID_passagem INTEGER,
-	
-	FOREIGN KEY (ID_pessoa) 
-    REFERENCES PESSOA (ID_pessoa)
-	
-	FOREIGN KEY (ID_passagem)
-    REFERENCES PASSAGEM (ID_passagem)
-);
-
-
-/* Cria tabela PESSOA_FUNCIONARIO */
-CREATE TABLE PESSOA_FUNCIONARIO 
-(
-    ID_funcionario INTEGER PRIMARY KEY,
-    ID_pessoa INTEGER,
-    ID_empresa INTEGER,
-	
 	FOREIGN KEY (ID_empresa)
-    REFERENCES EMPRESA (ID_empresa)
-	
+	REFERENCES EMPRESA (ID_empresa),
+
 	FOREIGN KEY (ID_pessoa)
-    REFERENCES PESSOA (ID_pessoa)
+	REFERENCES PESSOA (ID_pessoa)
 );
+
+
+
+/* Cria tabela ROTAS */
+CREATE TABLE ROTAS 
+(
+	ID_rota INTEGER PRIMARY KEY,
+	ID_hora_saida INTEGER,
+	ID_hora_chegada INTEGER,
+	ID_rodoviaria_saida INTEGER,
+	ID_rodoviaria_chegada INTEGER,
+	
+
+	FOREIGN KEY (ID_hora_saida) 
+	REFERENCES HORAS (ID_hora),
+
+	FOREIGN KEY (ID_hora_chegada)
+	REFERENCES HORAS (ID_hora),
+
+	FOREIGN KEY (ID_rodoviaria_chegada)
+	REFERENCES EMPRESA (ID_empresa),
+
+	FOREIGN KEY (ID_rodoviaria_saida)
+	REFERENCES EMPRESA (ID_empresa)
+);
+
+
+
+/* Cria tabela TIPO_CONTATO */
+CREATE TABLE TIPO_CONTATO 
+(
+	ID_tipo_contato INTEGER PRIMARY KEY,
+	tipo VARCHAR[30]
+);
+
+
+
+/* Cria tabela CONTATO */
+CREATE TABLE CONTATO 
+(
+	ID_contato INTEGER PRIMARY KEY,
+	ID_tipo_usuario INTEGER,
+	contato_registro INTEGER,
+	ID_tipo_de_contato INTEGER,
+	
+	
+	FOREIGN KEY (ID_contato)
+	REFERENCES PESSOA (ID_pessoa),
+
+	FOREIGN KEY (ID_contato)
+	REFERENCES EMPRESA (ID_empresa),
+
+	FOREIGN KEY (ID_tipo_de_contato)
+	REFERENCES TIPO_CONTATO (ID_tipo_contato)
+);
+
+
+
+/* Cria tabela HORAS */
+CREATE TABLE HORAS 
+(
+	ID_hora INTEGER PRIMARY KEY,
+	hora VARCHAR[30]
+); 
